a5de8c502ab345b5d40bc535b452a23cc9ef4867
shadmanhossain <shadmanh@sfu.ca>
2024-08-09 20:33:38 -0700

    Cleaning up redundant code

--- react-personal-portfolio-app/src/ResumePage.tsx
+++ react-personal-portfolio-app/src/ResumePage.tsx
@@ -4,10 +4,7 @@ export default function ResumePage() {
   return (
     <div className="resume-page">
       <div id="resume-page__title">
-        <h2>Experience</h2>
-      </div>
-      <div id="resume-page__technologies">
-        <h3>Technologies:</h3>
+        <h2>/experience</h2>
       </div>
     </div>
   );
