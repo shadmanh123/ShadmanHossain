a05577bda33ad3024c436747c14cb9fcde5731ef
shadmanhossain <shadmanh@sfu.ca>
2024-08-12 20:09:06 -0700

    Made Navbar stay above all other content, improved UI for cards

---
+++ react-personal-portfolio-app/src/Card.tsx
@@ -0,0 +1,34 @@
+import "./Card.css";
+
+export default function Card({ title, description }) {
+  return (
+    <div className="card">
+      <div className="card__contents">
+        <div className="card__contents__front">
+          <div className="card__contents__front__content">
+            <h1>{title}</h1>
+            <p>Hover Me</p>
+          </div>
+        </div>
+        <div className="card__contents__back">
+          <div className="card__contents__back__image">
+            <div className="card__contents__back__image__circle"></div>
+            <div
+              className="card__contents__back__image__circle"
+              id="right"
+            ></div>
+            <div
+              className="card__contents__back__image__circle"
+              id="bottom"
+            ></div>
+          </div>
+          <div className="card__contents__back__content">
+            <div className="description-content">
+              <p>{description}</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
--- react-personal-portfolio-app/src/ResumePage.tsx
+++ react-personal-portfolio-app/src/ResumePage.tsx
@@ -1,5 +1,6 @@
 import "./ResumePage.css";
 import Experience from "./Experience";
+import Card from "./Card";
 export default function ResumePage() {
   return (
     <div className="resume-page">
@@ -11,6 +12,32 @@ export default function ResumePage() {
       </div>
       <div id="resume-page__projects--title">
         <h1>/projects</h1>
+      </div>
+      <div id="resume-page__projects">
+        <Card
+          title={"AI Pac-Man"}
+          description={
+            "Developed an advanced Python-based AI agent modelled after Pac-Man that learns from its mistakes and improves gameplay strategy to achieve the highest score in new mazes with varying numbers of ghosts"
+          }
+        />
+        <Card
+          title={"Guess-The-Era Game"}
+          description={
+            "Developed a Kotlin app for the photo-based game, where users determine the year or decade of photos for points, compete against other players in real-time, and view friends’ scores in a leaderboard"
+          }
+        />
+        <Card
+          title={"Game Score Calculator"}
+          description={
+            "Engineered a user-friendly Java app offering users a comprehensive platform to accurately compute and monitor scores based on specific parameters inherent to each game’s scoring system"
+          }
+        />
+        <Card
+          title={"Trip Planner"}
+          description={
+            "Constructed a database using MySQL to plan travel itineraries, including attractions, currencies, and scheduling options, while providing information on accommodations and risk assessment for a tailored trip experience"
+          }
+        />
       </div>
     </div>
   );
