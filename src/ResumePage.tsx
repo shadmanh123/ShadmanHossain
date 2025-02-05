4e0251975a5cf0fa2cd3e029ce9ee94d6bad2aba
shadmanhossain <shadmanh@sfu.ca>
2025-02-04 19:51:21 -0800

    Standardizing title sections

--- src/ResumePage.tsx
+++ src/ResumePage.tsx
@@ -4,13 +4,13 @@ import Card from "./Card";
 export default function ResumePage() {
   return (
     <div className="resume-page">
-      <div id="resume-page__experience--title">
+      <div className="title">
         <h1>/experience</h1>
       </div>
       <div id="resume-page__experience">
         <Experience />
       </div>
-      <div id="resume-page__projects--title">
+      <div className="title">
         <h1>/projects</h1>
       </div>
       <div id="resume-page__projects">
